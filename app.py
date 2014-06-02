3d23746c1ea944ce13a8b8d5b75f43d21a2f0669
Cesar Carruitero <cesar@mozilla.pe>
2014-06-01 20:21:08 -0500

    redirect to index on 404

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, make_response
 
 app = Flask(__name__)
 app.config.from_pyfile('local_settings.py')
 
 @app.route('/')
 def index():
-    return render_template('indx.html')
+    return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(render_template('index.html'), 302)
 
 if __name__ == '__main__':
     app.run()
